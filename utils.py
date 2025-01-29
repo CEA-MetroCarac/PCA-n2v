676063a5d896102701115519639d1b58cae157bc
Matthew Bryan <matthewbryan52@gmail.com>
2025-01-29 09:41:45 +0100

    Typing and pathlib

--- utils.py
+++ utils.py
@@ -4,13 +4,16 @@ import numpy as np
 from n2v.internals.N2V_DataGenerator import N2V_DataGenerator
 from scipy import sparse
 import os
+import pathlib
 from tifffile import imwrite, imread
 from sklearn.preprocessing import StandardScaler
 from sklearn.decomposition import PCA
 import pandas as pd
+from typing import Tuple, Optional
+from typing_extensions import Literal
 
 
-def memory_alloc(n_go):
+def memory_alloc(n_go: int):
     """
     Allocate memory on the GPU. If not activated the code will
     by default fill the GPU, without any performance gain.
@@ -30,7 +33,7 @@ def memory_alloc(n_go):
             print(e)
 
 
-def reshape_2d(data):
+def reshape_2d(data: np.ndarray):
     """Reshape a 3d of 4d matrix into a 2d matrix."""
     mz = data.shape[-1]
     n_pix = int(data.size / mz)
@@ -38,62 +41,83 @@ def reshape_2d(data):
     return data
 
 
-def tif_to_matrix(dir_tif, extension="npz"):
+def tif_to_matrix(dir_tif: os.PathLike, as_sparse: bool = True):
     """
     Loads a dataset saved as a tif or tiff files in
-    dir_tif and transforms it to a npy or npz matrix.
+    dir_tif and transforms it to a npy or sparse matrix.
     """
-    list_im = os.listdir(dir_tif)
-    list_im = np.sort(list_im)
+    dir_tif = pathlib.Path(dir_tif)
+    list_im = [
+        f for f in dir_tif.iterdir()
+        if f.suffix in (".tif", ".tiff")
+    ]
     data = [
-        imread(os.path.join(dir_tif, f_name))
-        for f_name in list_im
-        if ".tif" in f_name
+        imread(f_name)
+        for f_name
+        in sorted(list_im, key=lambda f: f.stem)
     ]
     data = np.array(data)
     data = np.moveaxis(data, 0, -1)
-    if extension == "npz":
-        data = sparse.csr_matrix(reshape_2d(data))
+    if as_sparse:
+        return sparse.csr_matrix(reshape_2d(data))
     return data
 
 
-def npz_to_tif(data_npz, shape, dir_out):
+def npz_to_tif(data_npz: sparse.csr_matrix, shape: Tuple[int, ...], dir_out: os.PathLike):
     """
     Saves the elements from a npz matrix into an output
     directory (dir_out) as a tiff file.
     The last dimension of the matrix is assumed to be the channel axis.
     """
-    data = sparse.csr_matrix.todense(data_npz)
+    dir_out = pathlib.Path(dir_out)
+    data = data_npz.todense()
     data = np.array(data).reshape(shape)
     data = np.moveaxis(data, -1, 0)
-    data = list(data)
     for i, im in enumerate(data):
-        imwrite(os.path.join(dir_out, f"{i:04}.tiff"), im)
+        imwrite(dir_out / f"{i:04}.tiff", im)
 
 
-def iontof_to_matrix(dir_csv, dir_out, d_type="2d", extension="npz"):
-    """Takes as input a file where IonTof data has been saved as txt file.
-    Returns and saves the data in the file as a npy or npz file."""
-    list_csv = os.listdir(dir_csv)
-    # data = np.zeros(shape=())
-    for f_name in list_csv:
-        data_full = []
-        if "txt" in f_name:
+def iontof_to_matrix(
+    dir_csv: os.PathLike,
+    dir_out: os.PathLike,
+    d_type: Literal["2d", "3d"] = "2d",
+    extension: Optional[Literal["npz", "npy"]] = "npz",
+):
+    """
+    Takes as input a file where IonTof data has been saved as txt file.
+    Returns and saves the data in the file as a npy or npz file.
+    """
+    data_full = []
+    for f_name in pathlib.Path(dir_csv).iterdir():
+        if f_name.suffix == ".txt":
             data_mz, shape = txt_to_matrix(f_name, d_type)
             data_full.append(data_mz)
-    # mz = len(data_full)
+    if len(data_full) == 0:
+        raise RuntimeError("No .txt data found")
     data_full = np.array(data_full)
     data_full = np.moveaxis(data_full, 0, -1)
-    if extension == "npz":
-        sparse.save_npz(os.path.join(dir_out, "data.npz"), sparse.csr_matrix(data_full))
-    elif extension == "npy":
-        # shape = shape + (mz,)
-        # data = data.reshape(shape)
-        np.save(os.path.join(dir_out, "data.npy"), data_full)
+    dir_out = pathlib.Path(dir_out)
+    if extension is not None:
+        if extension == "npz":
+            sparse.save_npz(
+                dir_out / "data.npz",
+                sparse.csr_matrix(data_full)
+            )
+        elif extension == "npy":
+            np.save(
+                dir_out / "data.npy",
+                data_full
+            )
+        else:
+            raise ValueError(f"Unrecognized extension {extension}")
     return data_full
 
 
-def txt_to_matrix(f_csv, d_type="2d", dir_save=None):
+def txt_to_matrix(
+    f_csv: os.PathLike,
+    d_type: Literal["2d", "3d"] = "2d",
+    dir_save: Optional[os.PathLike] = None,
+):
     """
     Transforms a txt file exported from SurfaceLab into a matrix."""
     if d_type == "2d":
@@ -103,7 +127,7 @@ def txt_to_matrix(f_csv, d_type="2d", dir_save=None):
         names = ["x", "y", "z", "i"]
         dtype = {"x": int, "y": int, "z": int, "i": float}
     else:
-        print("Data type not supported")
+        raise ValueError(f"Data type not supported {d_type}")
 
     data_mz = pd.read_csv(
         f_csv,
@@ -130,26 +154,30 @@ def txt_to_matrix(f_csv, d_type="2d", dir_save=None):
 
     data_temp = data_temp.flatten()
     if dir_save is not None:
+        filename = pathlib.Path(f_csv).stem
         sparse.save_npz(
-            os.path.join(dir_save, f"{f_csv}.npy"),
+            pathlib.Path(dir_save) / f"{filename}.npy",
             sparse.csr_matrix(data_temp)
         )
     return data_temp, shape
 
 
-def load_data(f_name):
+def load_data(f_name: os.PathLike):
     """
     Loads a MSI dataset saved as a .npz sparse matrix.
     """
     data = sparse.load_npz(f_name)
-    data = np.array(sparse.csr_matrix.todense(data))
-    return data
+    return np.array(data.todense())
 
 
-def compute_scores(data, f_dir, d_name, save=True):
-    print("Computing scores...")
-
-    os.makedirs(f_dir, exist_ok=True)
+def compute_scores(
+    data: np.ndarray,
+    f_dir: os.PathLike,
+    d_name: str,
+    save: bool = True
+):
+    f_dir = pathlib.Path(f_dir)
+    f_dir.mkdir(parents=True, exist_ok=True)
     data = reshape_2d(data)
 
     # We now scale our data and compute the scores.
@@ -158,36 +186,37 @@ def compute_scores(data, f_dir, d_name, save=True):
     pca = PCA()
     scores = pca.fit_transform(data)
     if save:
-        np.save(os.path.join(f_dir, f"{d_name}_scores.npy"), scores.astype("float16"))
         np.save(
-            os.path.join(f_dir, f"{d_name}_components.npy"),
-            pca.components_.astype("float16"),
+            f_dir / f"{d_name}_scores.npy",
+            scores.astype("float16")
+        )
+        np.save(
+            f_dir / f"{d_name}_components.npy",
+            pca.components_.astype(np.float16),
         )
     # We reshape the scores.
     return scores
 
 
 def denoise_scores(
-    j,
-    scores_j,
-    batch_size,
-    basedir,
-    model_name,
-    patch_shape=None,
-    train_epochs=100,
-    split_val=0.9,
+    j: int,
+    scores_j: np.ndarray,
+    batch_size: int,
+    basedir: os.PathLike,
+    model_name: str,
+    patch_shape: Optional[Tuple[int, ...]] = None,
+    train_epochs: int = 100,
+    split_val: float = 0.9,
 ):
-    print(f"Denoising score {j}...")
-    datagen = N2V_DataGenerator()
-    dir_model = os.path.join(basedir, f"{model_name}")
-    dir_history = os.path.join(dir_model, "history")
-    dir_denoised = os.path.join(dir_model, "denoised_scores")
-    os.makedirs(dir_model, exist_ok=True)
-    os.makedirs(dir_denoised, exist_ok=True)
-    os.makedirs(dir_history, exist_ok=True)
+    basedir = pathlib.Path(basedir)
+    dir_model = basedir / model_name
+    dir_history = dir_model / "history"
+    dir_denoised = dir_model / "denoised_scores"
+    dir_model.mkdir(exist_ok=True)
+    dir_denoised.mkdir(exist_ok=True)
+    dir_history.mkdir(exist_ok=True)
 
     dat_j = data_augment(scores_j)
-
     dat_j = [np.expand_dims(dat, 0) for dat in dat_j]
     dat_j = [np.expand_dims(dat, -1) for dat in dat_j]
 
@@ -200,10 +229,11 @@ def denoise_scores(
             axes = "ZYX"
 
     # Generate patches
+    datagen = N2V_DataGenerator()
     patches = datagen.generate_patches_from_list(dat_j, shape=patch_shape)
 
     np.random.shuffle(patches)
-    patches = patches.astype("float32")  # float16 generates errors
+    patches = patches.astype(np.float32)  # float16 generates errors
     train_break = int(patches.shape[0] * split_val)
     X = patches[:train_break]
     X_val = patches[train_break:]
@@ -225,14 +255,14 @@ def denoise_scores(
     )
 
     # We are now creating our network model.
-    model = N2V(config, model_name, basedir=basedir)
+    model = N2V(config, model_name, basedir=str(basedir))
 
     # Start training. The model saves itself automatically.
     history = model.train(X, X_val)
 
     # Save history and denoised images
     history = pd.DataFrame.from_dict(history.history)
-    history.to_csv(os.path.join(dir_history, f"history_{j:04}.csv"))
+    history.to_csv(dir_history / f"history_{j:04}.csv")
 
     if len(patch_shape) == 2:
         axes = "YX"
@@ -240,15 +270,17 @@ def denoise_scores(
         axes = "ZYX"
 
     score_dn = model.predict(scores_j, axes)
-    imwrite(os.path.join(dir_denoised, f"score_{j:04}.tiff"), score_dn)
+    imwrite(dir_denoised / f"score_{j:04}.tiff", score_dn)
 
 
-def reconstruct(data, dir_scores_dn, f_components):
+def reconstruct(
+    data: np.ndarray,
+    dir_scores_dn: os.PathLike,
+    f_components: os.PathLike,
+) -> np.ndarray:
     """Reconstruct the dataset into a 2d matrix of dimensions (n_pix, n_mz)"""
 
-    print("Reconstructing data...")
-
-    data = data.astype("uint32")
+    data = data.astype(np.uint32)
     data = reshape_2d(data)
     means = np.mean(data, axis=1)
     std = np.std(data, axis=1)
@@ -268,9 +300,9 @@ def reconstruct(data, dir_scores_dn, f_components):
     return data_dn
 
 
-def data_augment(data):
+def data_augment(data: np.ndarray):
     # This function augments the data by performing flips and transpositions.
-    print("Augmenting data...")
+    # print("Augmenting data...")
     if data.ndim == 2:  # data times 8
         res = [np.rot90(data, k=i) for i in range(4)]
         for dat in [np.rot90(data, k=i) for i in range(4)]:
@@ -287,5 +319,5 @@ def data_augment(data):
         for dat in res2:
             res = res + [np.fliplr(dat)]
         res = res + res2
-    print(f"Data was augmented by a factor {len(res)}.")
+    # print(f"Data was augmented by a factor {len(res)}.")
     return res
